b94b3af46944131edbd32362fda2de6a650d5b4f
Collin... <collin.loertscher@dealertrack.com>
2016-10-20 20:24:10 -0600

    Created the boards.

--- Board/Board.cs
+++ Board/Board.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Board
+public class Board
 {
-    public class Board
+    private int[,] gameboard;
+    public Board(int coords1, int coords2)
     {
+        gameboard = new int[coords1,coords2];
     }
 }
--- Pieces/Pieces.cs
+++ Pieces/Pieces.cs
@@ -4,9 +4,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pieces
+public class Pieces
 {
-    public class Pieces
+    private string name;
+    private int rank;
+    //image
+    private Boolean bomb;
+    private Boolean flag;
+
+    public Pieces(int num)
     {
+        if(num == 1)
+        {
+            name = "Marshall";
+            rank = 1;
+            //image = newImage;
+            bomb = false;
+            flag = false;
+        }
+
+        if (num == 2)
+        {
+            name = "General";
+            rank = 2;
+            //image = newImage;
+            bomb = false;
+            flag = false;
+        }
+
+        if (num == 3)
+        {
+            name = "Colonels";
+            rank = 3;
+            //image = newImage;
+            bomb = false;
+            flag = false;
+        }
+
+        if (num == 4)
+        {
+            name = "Majors";
+            rank = 4;
+            //image = newImage;
+            bomb = false;
+            flag = false;
+        }
+
+        if (num == 5)
+        {
+            name = "Captains";
+            rank = 5;
+            //image = newImage;
+            bomb = false;
+            flag = false;
+        }
+
+        if (num == 6)
+        {
+            name = "Lieutenants";
+            rank = 6;
+            //image = newImage;
+            bomb = false;
+            flag = false;
+        }
+
+        if (num == 7)
+        {
+            name = "Sargeant";
+            rank = 7;
+            //image = newImage;
+            bomb = false;
+            flag = false;
+        }
+
+        if (num == 8)
+        {
+            name = "Miners";
+            rank = 8;
+            //image = newImage;
+            bomb = false;
+            flag = false;
+        }
+
+        if (num == 9)
+        {
+            name = "Scout";
+            rank = 9;
+            //image = newImage;
+            bomb = false;
+            flag = false;
+        }
+
+        if (num == 10)
+        {
+            name = "Spy";
+            rank = 10;
+            //image = newImage;
+            bomb = false;
+            flag = false;
+        }
+
+        if (num == 11)
+        {
+            name = "Bomb";
+            rank = 11;
+            //image = newImage;
+            bomb = true;
+            flag = false;
+        }
+
+        if (num == 12)
+        {
+            name = "Flag";
+            rank = 12;
+            //image = newImage;
+            bomb = false;
+            flag = true;
+        }
     }
 }
